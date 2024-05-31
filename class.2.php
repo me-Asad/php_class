2597f6a3aec21c6462cfd1cc80989cc87f4917c0
me-Asad <162339255+me-Asad@users.noreply.github.com>
2024-05-31 20:25:03 +0500

    Add files via upload

---
+++ class.2.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+// $admin = "Abdullah";
+// $pass = 12345;
+// $admin_02 = "Maaz";
+// $pass_02 = 12346;
+
+// switch ($admin == "Abdullah" && $pass == 12345 || $admin_02 == "Maaz" && $pass_02 == 123456) {
+//     case true:
+//         echo("Welcome");
+//         break;
+
+//     default:
+//     echo("Login Denied");
+//         break;
+// }
+
+// for ($i = 0; $i < 11; $i++) {
+//     if ($i == 5) {
+//         continue;
+//     }
+//     echo ("<h1>This is For Loop</h1>".$i ." <br>");
+// }
+
+// for ($i = 1; $i <= 5; $i++) {
+//     for ($j = 1; $j <= 3; $j++) {
+//         echo ("ye i hai " . $i . " ye j h  " . $j . "<br>");
+//     }
+
+// }
+
+// $a = 0;
+// while ($a <= 10) {
+//     if ($a = 5) {
+//      break;
+//     }
+//     echo ("<h1>This is While Loop</h1>". $a. "<br>");
+//     $a++;
+// }
+
+
+nas
