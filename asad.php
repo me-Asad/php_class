f90bac5b533bfbc66fe92718282fbad9a82c54fc
me-Asad <162339255+me-Asad@users.noreply.github.com>
2024-05-31 20:25:21 +0500

    Add files via upload

---
+++ asad.php
@@ -0,0 +1,25 @@
+<?php
+// echo("php");
+
+// $num =10;
+// $num_1 = 20;
+// echo($num+$num_1);
+
+// echo("i am progaming");
+
+// $num =10;
+// $num_1 = 20;
+// echo($num-$num_1);
+
+// $num =10;
+// $num_1 = 20;
+// echo($num>$num_1);
+
+// $num =10;
+// $num_1 = 20;
+// echo($num<$num_1);
+
+// echo("<script>alert('php programing');</script>");
+// $show = "php program";
+// echo($show);
+?>
