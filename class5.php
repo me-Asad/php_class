356ad60fe84401a39e46b35aca6d24527b50a2eb
me-Asad <162339255+me-Asad@users.noreply.github.com>
2024-06-03 20:38:58 +0500

    Add files via upload

---
+++ class5.php
@@ -0,0 +1,29 @@
+<?php
+// function Functionn()  {
+//     echo ("This is function");
+// Functionn()
+// }
+
+$my_array = array("black", "pink", 'blue','red',"white");
+// $my_array = array("PHP", "Programming", 'false', 50,["Sikandar",15]);
+// echo($my_array[0]);
+// // $my_array[3] = "Replace";
+// echo($my_array[1]);
+// echo($my_array[2]);
+// echo($my_array[3]);
+// echo($my_array[4]);
+// $my_array[4]();
+// $countt = count($my_array);
+// // echo($countt);
+// echo $my_array[4][0];
+// echo $my_array[4][1];
+// foreach ($my_array as $z){
+// echo "<h1 style='color: pink'> $z </h1>" . "<br>";
+// }
+// $my_array_ = array("year"=> 2022, "car" => "corolla");
+// echo ["Programming"];
+//  echo $my_array_["year"];
+// $my_array[4] = "<h1>jo larkiya khud ko samajti ha aliya unko milta hai kaliya</h1>";
+
+// echo $my_array[4];
+// ?>
