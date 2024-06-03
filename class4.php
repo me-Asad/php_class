9a821499a6ab1767510f28927181322767dffa46
me-Asad <162339255+me-Asad@users.noreply.github.com>
2024-06-03 20:08:34 +0500

    Add files via upload

---
+++ class4.php
@@ -0,0 +1,35 @@
+<?php
+// $num = 10;
+// for($i=1;$i<=10; $i++)
+// {
+//     echo $num ."x" . "$i" ."=" . $num*$i;
+//     echo "</br>";    
+// }
+
+// $number = 3;
+// if ($number % 2 == 0 ){
+//     echo "$number is an evenumber."; 
+// }
+// else {
+//     echo "$number is an odd number.";
+// }
+
+// for($i = 1; $i <= 100 ; $i++)
+// { 
+//     if ($i % 3 == 0 && $i % 5 == 0){ 
+//         echo"fizzbuzz</br>";
+//     } elseif ($i % 3 == 0 ){
+//         echo "fizz</br>";
+//     }      elseif ($i % 3 == 0 ){
+//         echo "Buzz</br>";
+
+//     } else {
+//         echo $i . "</br>";
+//     }
+
+// }
+
+
+
+
+?>
